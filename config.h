f607b3c39a38667199c92be2024e04e83aa966c4
iamdarshg <iamdarshg@gmail.com>
2025-03-17 18:46:31 +0530

    going gym update 17/3

--- config.h
+++ config.h
@@ -1,10 +1,3 @@
-
-/*
- * Copyright Freddie Chopin 2013.
- * Distributed under the Boost Software License, Version 1.0.
- * (See accompanying file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
- */
-
 /**
  * \file config.h
  * \brief Basic configuration of the project
